340966dd6320566e73f3251e03cb9acd520f67dc
Al Johri <al.johri@gmail.com>
2015-06-25 19:16:11 -0400

    use grunt-cli not grunt

--- mac.bash
+++ mac.bash
@@ -327,7 +327,7 @@ nvm install "$node_version"
 fancy_echo "Setting $node_version as the global default nodejs..."
 nvm alias default "$node_version"
 
-npm install -g yo generator-angular bower grunt
+npm install -g yo generator-angular bower grunt-cli
 
 ################
 
