d590dfbe4f9cc32a54b79cece600c83fba3b745e
Al Johri <al.johri@gmail.com>
2015-09-08 04:34:14 -0400

    modify node script

--- node.sh
+++ node.sh
@@ -6,8 +6,13 @@ node_version="0.12.4"
 mkdir -p "$HOME/.nvm"
 cp "$(brew --prefix nvm)/nvm-exec" "$HOME/.nvm/"
 source "$(brew --prefix nvm)/nvm.sh"
-nvm install "$node_version"
-fancy_echo "Setting $node_version as the global default nodejs..."
-nvm alias default "$node_version"
 
-npm install -g yo generator-angular bower grunt-cli
+if ! nvm ls | grep -Fq "$node_version"; then
+  nvm install "$node_version"
+  fancy_echo "Setting $node_version as the global default nodejs..."
+  nvm alias default "$node_version"
+fi
+
+yo --version || npm install -g yo generator-angular
+bower --version || npm install -g bower
+grunt --version || npm install -g grunt-cli
